9eb1f61cb260bddf976b634a58e710854e4ab711
Sudeep Sahu <sudeepsahu2027@gmail.com>
2026-03-08 21:52:37 +0530

    changes cookie fun

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -99,13 +99,18 @@ async function loginUserController(req, res) {
     });
   }
 
-  const token = jwt.sign(
-    { id: user._id, username: user.username },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" },
-  );
+   const token = jwt.sign(
+       { id: user._id, username: user.username },
+       process.env.JWT_SECRET,
+       { expiresIn: "1d" },
+     );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+  httpOnly: true,        
+  secure: true,          
+  sameSite: "none",      
+  maxAge: 24 * 60 * 60 * 1000, 
+});
 
   res.status(200).json({
     message: "User loggedin successfully",
@@ -129,7 +134,7 @@ async function loginUserController(req, res) {
  */
 async function logoutUserController(req,res) {
   const token=req.cookies.token;
-  console.log(token);
+  
 
   if(token){
     await blacklistModel.create({token});
